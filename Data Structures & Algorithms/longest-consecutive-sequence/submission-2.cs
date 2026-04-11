8df5d0068434fa20f1a1357097d5cc11f4aebf27
ethanbbaker <121688174+ethanbbaker@users.noreply.github.com>
2026-04-11 13:09:49 -0600

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+        if(nums.Length == 0) return 0;
+        Array.Sort(nums);
+        int j = 1; //working count
+        int k = 0; //highest count
+
+        for(int i = 0; i<nums.Length - 1;i++){
+            if(nums[i] == nums[i+1]){
+                continue;
+            }
+            if(nums[i] == (nums[i+1] - 1)){
+                j++;
+            }
+            else{
+                if(j > k){
+                    k = j;
+                    j = 1;
+                }
+                else{
+                    j = 1;
+                }
+            }
+        }
+        if(j > k) return j;
+        return k;
+    }
+}
